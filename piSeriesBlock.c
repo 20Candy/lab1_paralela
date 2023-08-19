81b7812cb032f865fbd48bc8c4049728bcc0b2fe
stefanoaragoni <stefanoaragoni13@gmail.com>
2023-08-19 17:11:50 -0600

    update

--- piSeriesBlock.c
+++ piSeriesBlock.c
@@ -7,10 +7,34 @@
 
 int main() {
     int n = 10e6;  
-    int thread_count = omp_get_num_procs();;   // 4 cores tiene la maquina virtual
+    int thread_count = omp_get_num_procs();  // retorna el número de procesadores de la maquina virtual
     double pi_approx;
-    
-    printf("Número de hilos: %d\n", thread_count);
+
+    omp_sched_t scheduleType = 0;
+    int chunkSize = 16;
+
+    omp_set_schedule(scheduleType, chunkSize); // politica de planificacion y tamaño de bloque
+
+    for (int i = 0; i < 5; i++) {
+
+        double sum = 0.0;
+        double factor = 1.0;
+        double start_time, end_time;
+
+        start_time = omp_get_wtime();
+
+        #pragma omp parallel for num_threads(thread_count) schedule(runtime) reduction(+:sum) private(factor)
+        for (int k = 0; k < n; k++) {
+            factor = (k % 2 == 0) ? 1.0 : -1.0;
+            sum += factor / (2.0 * k + 1.0);
+        }
+
+        end_time = omp_get_wtime();
+
+        pi_approx = 4.0 * sum;
+        printf("Valor de PI: %.20f, Tiempo: %f\n", i + 1, pi_approx, end_time - start_time);
+
+    }
 
     return 0;
 }
